18945ae074af78c8061e5e7714500de757e795f9
userer <ilya.qaheri@live.com>
2016-09-16 18:11:15 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {130962237,202482661},--Sudo users
+    sudo_users = {130962237},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
